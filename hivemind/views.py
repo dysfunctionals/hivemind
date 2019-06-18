c6b03bdc202d05cec254403fe6baf0f7588816d0
Dan Trickey <dan@trickey.io>
2019-06-18 15:33:33 +0100

    sdfklo:wq

--- hivemind/views.py
+++ hivemind/views.py
@@ -2,7 +2,9 @@ from hivemind import app
 from flask import flash, redirect, render_template, request, url_for
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         return render_template("index.html")
+    else:
+        return "OOF"
