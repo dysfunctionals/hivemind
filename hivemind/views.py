b2b1a2949e768916c381a0ff3a6543c7283cd2b6
Dan Trickey <dan@trickey.io>
2019-06-18 16:03:35 +0100

    fd:wq

--- hivemind/views.py
+++ hivemind/views.py
@@ -1,10 +1,22 @@
 from hivemind import app
 from flask import flash, redirect, render_template, request, url_for
 
+from mcipc.query import Client as QClient
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template("index.html")
+        
+        with QClient("diseased.horse", 25565) as q:
+            stats = q.full_stats
+        
+        return render_template(
+            "index.html",
+            status="Unknown",
+            online_players=stats["num_players"],
+            total_players=stats["max_players"],
+            motd=status["hostname"],
+            players=["OO"],
+        )
     else:
-        return "OOF"
+        return str(request.form)
